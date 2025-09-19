3f4db8fb821c14fcaf51aafb8d4b3d1a7077aa19
Bashiru200 <bashirubarrie12@gmail.com>
2025-09-18 20:50:54 -0400

    using component

--- alx-project-0x00/components/Card.tsx
+++ alx-project-0x00/components/Card.tsx
@@ -1,17 +1,15 @@
 import  Image from "next/image"
-import HOUSE_IMAGE from "@/public/assets/house.jpg"
-import STAR_IMAGE from "@/public/assets/star.png"
 import React from "react"
 import Pill from "./Pill"
 
 const Card: React.FC = () => {
     return (
         <div className="h-[422px] w-[378.56px] cursor-pointer hover:shadow-md hover:rounded-lg">
-            <Image className="rounded-lg" src={HOUSE_IMAGE} width={378.56} height={299.37} alt="house image" />
+            <Image className="rounded-lg" src="/assets/images/house.png" width={378.56} height={299.37} alt="house image" />
             <div className="p-2 flex gap-2 mt-2">
                 <Pill title="Top Villa" />
                 <Pill title="Self CheckIn" />
-                <Pill title="Free Reschedule" />"
+                <Pill title="Free Reschedule" />
             </div>
             <div className="flex items-center <justify-between">
                 <div>
@@ -19,7 +17,7 @@ const Card: React.FC = () => {
                     <p className="font-medium text-[17px] text-[#929292]">Sideman, Bali, Indonesia</p>
                 </div>
                 <div className=" flex items-center">
-                    <Image src={STAR_IMAGE} alt="start" />"
+                    <Image src="/assets/images/star.png" width={14.56} height={12.37} alt="start" />
                     <p className="font-medium text-[17px] ml-2">4.76</p>
                 </div>
             </div>
